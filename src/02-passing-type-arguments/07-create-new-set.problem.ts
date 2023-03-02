abf07c2b3ed73c3d61a870163395ed5e954bae1e
Dusan Misic <dmisic@inventage.com>
2023-03-02 15:36:12 +0100

    exercise 7: Use type arguments.

--- src/02-passing-type-arguments/07-create-new-set.problem.ts
+++ src/02-passing-type-arguments/07-create-new-set.problem.ts
@@ -1,7 +1,11 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-export const createSet = () => {
-  return new Set();
+/**
+ * Here we can see that we can use generics without having a parameter on the function itself. This means that if you call the function
+ * you must pass a type argument, in order to use it, just as if the function itself had parameters.
+ */
+export const createSet = <T>() => {
+  return new Set<T>();
 };
 
 const stringSet = createSet<string>();
