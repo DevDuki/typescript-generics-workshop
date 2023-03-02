02a8dc041ca6b7f2afb68de86e53d8944fb7b7a6
Dusan Misic <dmisic@inventage.com>
2023-03-02 15:04:20 +0100

    exercise 3: Add extra info

--- src/01-generics-intro/03-multiple-generics.problem.ts
+++ src/01-generics-intro/03-multiple-generics.problem.ts
@@ -3,6 +3,9 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 /**
  * Here we can see that we can also use multiple generics
+ * Note! that here the return types are not as specific as before, when we used a single generic type. Notice in the test below how
+ * a: "a" has become a: string and b: 1 has become b: number. If you wish to get more specific return types, then you can do so by being
+ * more restrictive to the generic types. For example <T1 extends string, T2 extends number>.
  */
 const returnBothOfWhatIPassIn = <T1, T2>(a: T1, b: T2) => {
   return {
