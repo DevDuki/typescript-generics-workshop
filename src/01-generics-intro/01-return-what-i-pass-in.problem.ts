c378a152751370b204f9fd446b87734245a77a5e
Dusan Misic <dmisic@inventage.com>
2023-03-02 14:47:42 +0100

    exercise 1: Use a generic function to automatically infer the return type.

--- src/01-generics-intro/01-return-what-i-pass-in.problem.ts
+++ src/01-generics-intro/01-return-what-i-pass-in.problem.ts
@@ -1,7 +1,13 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnWhatIPassIn = (t: unknown) => {
-  return t;
+/**
+ * Here we basically create a kind of type function AROUND an actual function, to infer the return type.
+ *
+ * Kind of like this:
+ * type ReturnWhatIPassIn<T> = T
+ */
+const returnWhatIPassIn = <T>(param: T) => {
+  return param;
 };
 
 const one = returnWhatIPassIn(1);
