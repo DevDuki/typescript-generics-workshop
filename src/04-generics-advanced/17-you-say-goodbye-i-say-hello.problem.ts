7ac7984c52a6f29674f52b995f4791dc5287c99e
Dusan Misic <dmisic@inventage.com>
2023-03-03 13:11:11 +0100

    exercise 17: Use a conditional type as a return type in a function, with the "as" keyword.

--- src/04-generics-advanced/17-you-say-goodbye-i-say-hello.problem.ts
+++ src/04-generics-advanced/17-you-say-goodbye-i-say-hello.problem.ts
@@ -1,8 +1,13 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-function youSayGoodbyeISayHello(greeting: unknown) {
-  return greeting === "goodbye" ? "hello" : "goodbye";
+/**
+ * Here we see that we can use a conditional type as a return type of a function, but we have to use the "as" keyword to tell typescript
+ * that we are actually returning a conditional type. This is because the return type of a function is always a type, so we have to tell
+ * typescript that we are actually returning a conditional type.
+ */
+function youSayGoodbyeISayHello<T extends "hello" | "goodbye">(greeting: T) {
+  return (greeting === "goodbye" ? "hello" : "goodbye") as T extends "hello" ? "goodbye" : "hello";
 }
 
 it("Should return goodbye when hello is passed in", () => {
