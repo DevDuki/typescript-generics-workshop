aa36e509d5d3b4d218d0685c592a0e41fb9635c3
Dusan Misic <dmisic@inventage.com>
2023-03-03 15:15:54 +0100

    exercise 21: Intro to function overloads

--- src/05-function-overloads/21-what-is-a-function-overload.problem.ts
+++ src/05-function-overloads/21-what-is-a-function-overload.problem.ts
@@ -3,7 +3,13 @@ import { Equal, Expect } from "../helpers/type-utils";
 /**
  * This time, let's solve this with function overloads!
  */
-const returnWhatIPassIn = (t: unknown) => {
+
+/**
+ * Here we use function overloads to define the return type of the function based on the type of the argument that we pass in.
+ */
+function returnWhatIPassIn(t: 1): 1;
+function returnWhatIPassIn(t: "matt"): "matt";
+function returnWhatIPassIn(t: unknown) {
   return t;
 };
 
