d34e303ceadae84ee5efa532a419c553cef32c41
Dusan Misic <dmisic@inventage.com>
2023-03-03 11:28:49 +0100

    exercise 15: Make the return type more specific in members of an array.

--- src/03-art-of-type-arguments/15-array-or-array-member-in-generics.problem.ts
+++ src/03-art-of-type-arguments/15-array-or-array-member-in-generics.problem.ts
@@ -1,6 +1,14 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const makeStatus = <TStatuses extends string[]>(statuses: TStatuses) => {
+/**
+ * Here we see again that as a rule of thumb, it is again better to go lower with our type arguments. So instead of using "Array<string>" as
+ * extends to that type argument, we only use string and then in the function argument we represent it as an array. Now funnily enough, if
+ * we remove the "extends string" from the type argument, we get a more general return type of 'string[]' instead of 'Array<"INFO" |
+ * "DEBUG" | ...'. So we see again, like in the previous explainers, that if we further restrict the type argument, only then will the
+ * typescript compiler infer a more specific return type.
+ * @param statuses
+ */
+const makeStatus = <TStatus extends string>(statuses: TStatus[]) => {
   return statuses;
 };
 
