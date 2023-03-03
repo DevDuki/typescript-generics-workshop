61ea5512a3b6b26c4551db76be658584434d68ae
Dusan Misic <dmisic@inventage.com>
2023-03-03 14:20:40 +0100

    exercise 20: Spotting useless generics and replace them

--- src/04-generics-advanced/20-spotting-useless-generics.problem.ts
+++ src/04-generics-advanced/20-spotting-useless-generics.problem.ts
@@ -1,10 +1,13 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnBothOfWhatIPassIn = <T1, T2>(params: {
-  a: T1;
-  b: T2;
-}): [T1, T2] => {
+/**
+ * In this exercise we are only demonstrating, that we should always keep our eyes open for useless type arguments. Like here we had 2
+ * type arguments for each property "a" and "b", but we could have just used 1 type argument for both properties and then access each of
+ * them individually. I personally would have preferred the 2 type arguments, because it is less boilerplate-y, and it's faster to
+ * understand, what we were trying to do.
+ */
+const returnBothOfWhatIPassIn = <T1 extends { a: any, b: any }>(params: T1): [T1['a'], T1['b']] => {
   return [params.a, params.b];
 };
 
