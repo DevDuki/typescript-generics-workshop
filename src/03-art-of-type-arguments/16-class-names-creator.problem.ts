b697c007035e57ba194e4d508db91dbc12dc0a64
Dusan Misic <dmisic@inventage.com>
2023-03-03 12:39:51 +0100

    exercise 16: Use 2 different "scopes" to declare our type arguments, to solve the same problem. Fascinating.

--- src/03-art-of-type-arguments/16-class-names-creator.problem.ts
+++ src/03-art-of-type-arguments/16-class-names-creator.problem.ts
@@ -1,9 +1,32 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
+/**
+ * As recently we have 2 solutions again. So let's look at the first one first.
+ *
+ * Solution 1:
+ * Here we use our rule of thumb and try to go as low/deep as possible with our type argument. So here we say that our type argument
+ * "ClassName" extends a string, because we only care about the "keys" of the classes object, which are indeed type of string. So we
+ * then assign the argument "classes" of our function the type of Record<ClassName, string>, which means that the keys of the object, as
+ * mentioned before, are kinda "saved" in our type argument "ClassName". We can then assign this type to the argument "type" of our
+ * callback function and that's it! We got autocompletion and type safety for our "type" argument!
+ */
 const createClassNamesFactory =
-  (classes: unknown) =>
-  (type: unknown, ...otherClasses: unknown[]) => {
+  <ClassName extends string>(classes: Record<ClassName, string>) =>
+  (type: ClassName, ...otherClasses: string[]) => {
+    const classList = [classes[type], ...otherClasses];
+    return classList.join(" ");
+  };
+
+/**
+ * Solution 2:
+ * So here is a solution, where we sort of try to go higher with our type argument. So we say that our type argument "TClasses" extends
+ * a Record<string, string>. We then assign this to the "classes" function argument, and then we use the "keyof" operator to get the keys
+ * of TClasses. We then assign this to the "type" argument of our callback function. So we are going lower later on in the code instead.
+ */
+const createClassNamesFactory2 =
+  <TClasses extends Record<string, string>>(classes: TClasses) =>
+  (type: keyof TClasses, ...otherClasses: string[]) => {
     const classList = [classes[type], ...otherClasses];
     return classList.join(" ");
   };
