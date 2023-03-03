94c738cc9ea9cd2754ccc53dbc58a2dc1a5a85af
Dusan Misic <dmisic@inventage.com>
2023-03-03 13:42:43 +0100

    exercise 18: Use type arguments in curried functions.

--- src/04-generics-advanced/18-generic-currying.problem.ts
+++ src/04-generics-advanced/18-generic-currying.problem.ts
@@ -1,10 +1,21 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
+/**
+ * This example demonstrates that the generic type arguments are linked to each function call. So for instance:
+ *
+ * export const curryFunction =
+ * <T, U, V>(t: T) =>
+ *   (u: U) =>
+ *   (v: V) => { ... }
+ *
+ * Wouldn't work, because the type arguments U and V are linked to the first function call, and thus cannot refer anything there, which
+ * makes them unknown.
+ */
 export const curryFunction =
-  <T, U, V>(t: T) =>
-  (u: U) =>
-  (v: V) => {
+  <T>(t: T) =>
+  <U >(u: U) =>
+  <V>(v: V) => {
     return {
       t,
       u,
