d13b14f60e936795091b77f031e55a4ddd4c4126
Dusan Misic <dmisic@inventage.com>
2023-03-03 10:50:28 +0100

    exercise 14: Using the art of generics to give 2 possible ways of typing things more generic or more explicit. A really good one!

--- src/03-art-of-type-arguments/14-safe-function.problem.ts
+++ src/03-art-of-type-arguments/14-safe-function.problem.ts
@@ -1,14 +1,18 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const makeSafe =
-  (func: unknown) =>
-  (
-    ...args: unknown
-  ):
+/**
+ * Here we again have 2 solutions.
+ *
+ * Solution 1:
+ * The first solution I came up with, is also the first solution presented in the video. We use 2 type arguments, to kind of represent
+ * exactly what we are looking for. In our case we are looking for the result of the passed "func" function and the arguments that are
+ * passed to it. So we use 2 type arguments to represent that.
+ */
+const makeSafe = <TResult, TArgs extends Array<any>>(func: (...args: TArgs) => TResult) => (...args: TArgs):
     | {
         type: "success";
-        result: unknown;
+        result: TResult;
       }
     | {
         type: "failure";
@@ -22,6 +26,39 @@ const makeSafe =
         result,
       };
     } catch (e) {
+      return {
+        type: "failure",
+        error: e as Error,
+      };
+    }
+  };
+
+/**
+ * Solution 2:
+ * For the second solution we grab the whole passed in function in the type argument and use the utility types "Parameters" and "ReturnType"
+ * to get the desired types. This looks pretty cool, but then again, what do you prefer? A more explicit type argument or a more generic one?
+ * So this is completely up to you. In this case I personally prefer the second solution, because when you hover over the returned
+ * function when u use this makeSafe2, you can see the whole function that gets typed in the type argument. So it's also easier to
+ * understand what we are actually representing with the type argument.
+ */
+const makeSafe2 = <TFunc extends (...args: any[]) => any>(func: TFunc) => (...args: Parameters<TFunc>):
+    | {
+        type: "success";
+        result: ReturnType<TFunc>;
+      }
+    | {
+        type: "failure";
+        error: Error;
+      } => {
+    try {
+      const result = func(...args);
+
+      return {
+        type: "success",
+        result,
+      };
+    }
+    catch (e) {
       return {
         type: "failure",
         error: e as Error,
