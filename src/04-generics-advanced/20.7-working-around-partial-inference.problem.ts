9488b59ceb3cfe4e5ef5a70cacff492e45d9a0ee
Dusan Misic <dmisic@inventage.com>
2023-03-03 15:01:33 +0100

    exercise 20.7: Use a wrapper function again to first infer a type argument, after it's been called, so it can be used as a type inside the second function's type argument.

--- src/04-generics-advanced/20.7-working-around-partial-inference.problem.ts
+++ src/04-generics-advanced/20.7-working-around-partial-inference.problem.ts
@@ -1,13 +1,18 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-export const makeSelectors = <
-  TSource,
-  TSelectors extends Record<string, (source: TSource) => any> = {},
->(
-  selectors: TSelectors,
-) => {
-  return selectors;
-};
+/**
+ * Because typescript cannot infer TSource and then immediately use it as a type for a second type argument, like TSelectors, we have
+ * to use the same pattern as in the previous exercise, and wrap it in an identity function. That identity function will then take care
+ * of TSource, which gets inferred as soon as the user calls that function, and once the user calls the returned function, with the
+ * selectors. They will have the correct TSource type inferred.
+ */
+export const makeSelectors =
+  <TSource> () =>
+  <TSelectors extends Record<string, (source: TSource) => any> = {}>(
+    selectors: TSelectors,
+  ) => {
+    return selectors;
+  };
 
 interface Source {
   firstName: string;
@@ -27,7 +32,7 @@ interface Source {
  *
  * makeSelectors<Source>()({ ...selectorsGoHere })
  */
-const selectors = makeSelectors<Source>({
+const selectors = makeSelectors<Source>()({
   getFullName: (source) =>
     `${source.firstName} ${source.middleName} ${source.lastName}`,
   getFirstAndLastName: (source) => `${source.firstName} ${source.lastName}`,
