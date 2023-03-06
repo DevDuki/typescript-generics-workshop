f9f49c344dd4595ab9bf595fdd6a3fd4466dce62
Dusan Misic <dmisic@inventage.com>
2023-03-06 10:08:56 +0100

    challenge 29.2: Type arguments with conditional type and a named tuple.

--- src/06-challenges/29.2-dynamic-function-arguments.problem.ts
+++ src/06-challenges/29.2-dynamic-function-arguments.problem.ts
@@ -8,7 +8,12 @@ interface Events {
   focus: undefined;
 }
 
-export const sendEvent = (event: keyof Events, ...args: any[]) => {
+/**
+ * Nothing new here, except for what we have learned already, but interesting to see is the named tuple syntax in the conditional type
+ * ("[ payload: Events[TEventKey]"). This makes the name of the argument called "payload", so when we call the function which triggers
+ * the conditional to be that tuple, then you can access that tuple by using "payload" as the property name! Pretty cool!
+ */
+export const sendEvent = <TEventKey extends keyof Events>(event: TEventKey, ...args: Events[TEventKey] extends undefined ? [] : [payload: Events[TEventKey]]) => {
   // Send the event somewhere!
 };
 
