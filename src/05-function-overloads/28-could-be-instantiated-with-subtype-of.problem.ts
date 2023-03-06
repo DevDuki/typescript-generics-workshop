1abc1fc264cd1d96f1a4dbf3ffa33bf69675e920
Dusan Misic <dmisic@inventage.com>
2023-03-06 08:51:39 +0100

    exercise 28: Use function overload, to get default values for a generic type

--- src/05-function-overloads/28-could-be-instantiated-with-subtype-of.problem.ts
+++ src/05-function-overloads/28-could-be-instantiated-with-subtype-of.problem.ts
@@ -8,9 +8,17 @@ const obj = {
 
 type ObjKey = keyof typeof obj;
 
-const getObjValue = <TKey extends ObjKey>(key: TKey = "a") => {
+/**
+ * Here we refactored an arrow function into a function overload where we wanted to get rid of typescript's weird error message of
+ * "could be instantiated with a subtype of blablabla". This is because we were using a type argument, but then tried to give it a
+ * default value, which can actually be overridden by the function consumer, if they paas in a different type argument. So a solution
+ * for that is this construct below.
+ */
+function getObjValue(): typeof obj["a"];
+function getObjValue<TKey extends ObjKey>(key: TKey): typeof obj[TKey];
+function getObjValue(key: ObjKey = "a") {
   return obj[key];
-};
+}
 
 const one = getObjValue("a");
 const oneByDefault = getObjValue();
